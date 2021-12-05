cc569807462d0cb7bd0a9cfe210a29a93cad6a14
giacoletti <giacoletti92@gmail.com>
2021-12-05 22:30:15 +0100

    Adds end2end test to check if user is able to play a game by clicking on a card

---
+++ cypress/integration/userCanClickCardToPlay.feature.js
@@ -0,0 +1,14 @@
+describe('Visiting the application, a user', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('[data-cy=rps-card-1]').click();
+  });
+
+  it('is expected to open modal and display match result', () => {
+    cy.get('[data-cy=rps-modal]').within(() => {
+      cy.get('[data-cy=rps-match-result]').should('be.visible');
+      cy.get('[data-cy=rps-user-choice]').should('be.visible');
+      cy.get('[data-cy=rps-cpu-choice]').should('be.visible');
+    });
+  });
+});
