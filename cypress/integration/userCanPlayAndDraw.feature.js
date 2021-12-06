db5b6b8736ac4c967c6e0a386e612d1fa2d445c6
giacoletti <giacoletti92@gmail.com>
2021-12-06 21:55:47 +0100

    Adds end2end tests to check if user can play and lose or draw a match

---
+++ cypress/integration/userCanPlayAndDraw.feature.js
@@ -0,0 +1,34 @@
+describe('Visiting the application, a user', () => {
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.stub(win.Math, "floor").returns(0).as('mathStub');
+      }
+    });
+    cy.get('[data-cy=rps-card-1]').click();
+  });
+
+  describe('is expected to open modal and', () => {
+    it('is expected to call Math floor function once', () => {
+      cy.get('@mathStub').should('be.calledOnce');
+    });
+
+    it("is expected to display 'That's a draw!' message", () => {
+      cy.get('[data-cy=rps-modal]').within(() => {
+        cy.get('[data-cy=rps-match-result]').should('contain.text', "That's a draw!");
+      });
+    });
+
+    it('is expected to display user chosen rock card', () => {
+      cy.get('[data-cy=rps-user-choice]').within(() => {
+        cy.get('.image').should('have.attr', 'name', 'rock');
+      });
+    });
+
+    it('is expected to display cpu chosen scissor card', () => {
+      cy.get('[data-cy=rps-cpu-choice]').within(() => {
+        cy.get('.image').should('have.attr', 'name', 'rock');
+      });
+    });
+  });
+});
---
+++ cypress/integration/userCanPlayAndLose.feature.js
@@ -0,0 +1,34 @@
+describe('Visiting the application, a user', () => {
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.stub(win.Math, "floor").returns(1).as('mathStub');
+      }
+    });
+    cy.get('[data-cy=rps-card-1]').click();
+  });
+
+  describe('is expected to open modal and', () => {
+    it('is expected to call Math floor function once', () => {
+      cy.get('@mathStub').should('be.calledOnce');
+    });
+
+    it("is expected to display 'CPU wins!' message", () => {
+      cy.get('[data-cy=rps-modal]').within(() => {
+        cy.get('[data-cy=rps-match-result]').should('contain.text', "CPU wins!");
+      });
+    });
+
+    it('is expected to display user chosen rock card', () => {
+      cy.get('[data-cy=rps-user-choice]').within(() => {
+        cy.get('.image').should('have.attr', 'name', 'rock');
+      });
+    });
+
+    it('is expected to display cpu chosen scissor card', () => {
+      cy.get('[data-cy=rps-cpu-choice]').within(() => {
+        cy.get('.image').should('have.attr', 'name', 'paper');
+      });
+    });
+  });
+});
